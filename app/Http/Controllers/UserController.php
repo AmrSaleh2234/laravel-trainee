ec3da570edc996c4207b5fed1ca21ad25e3a6f7f
AmrSaleh2234 <amrsaleh1001@gmail.com>
2026-08-18 14:59:40 +0300

    fix a problem

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,12 +10,12 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    function register()
+    function register(Request $request)
     {
         $newUser = User::create([
             "name" => $request->input("name"),
             "email" => $request->input("email"),
-            "password" => $request->input("password")
+            "password" => Hash::make($request->input("password"))
         ]);
 
         return $newUser;
